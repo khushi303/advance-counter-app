2c90399272de7659a27dfb76846b55003589fdf4
khushi303 <maankhushi303@gmail.com>
2023-09-17 14:58:39 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("add");
+let p = document.getElementById("text");
+let buttons = document.getElementById("save");
+let saved = document.getElementById("saved");
+
+button.addEventListener("click", function () {
+    p.innerHTML = +p.innerHTML + 1;
+    saved.classList.remove("disable");
+});
+buttons.addEventListener("click", function () {
+    if (p.innerHTML == 0) {
+        saved.classList.add("disable");
+    } else if (saved.innerHTML == 0) {
+        saved.innerHTML = p.innerHTML;
+        p.innerHTML = 0
+    } else {
+        saved.innerHTML = saved.innerHTML + "," + p.innerHTML;
+        p.innerHTML = 0
+    }
+})
