fa2c3abe1687fb1e08ad49bbce93b6616930ad20
khushi303 <maankhushi303@gmail.com>
2023-09-18 15:47:40 +0530

    removing space

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,6 @@ let p = document.getElementById("text");
 let buttons = document.getElementById("save");
 let saved = document.getElementById("saved");
 let reset = document.getElementById("reset")
-
 button.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1;
     saved.classList.remove("disable");
@@ -19,7 +18,6 @@ buttons.addEventListener("click", function () {
         p.innerHTML = 0
     }
 })
-
 reset.addEventListener("click", function () {
     p.innerHTML = 0
     saved.innerHTML = 0
