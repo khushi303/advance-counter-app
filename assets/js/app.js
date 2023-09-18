b28b1cef2c42cf97acd5267fc8ca5d1063968111
khushi303 <maankhushi303@gmail.com>
2023-09-18 10:50:02 +0530

    adding reset btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ let button = document.getElementById("add");
 let p = document.getElementById("text");
 let buttons = document.getElementById("save");
 let saved = document.getElementById("saved");
+let reset = document.getElementById("reset")
 
 button.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1;
@@ -18,3 +19,9 @@ buttons.addEventListener("click", function () {
         p.innerHTML = 0
     }
 })
+
+reset.addEventListener("click", function () {
+    p.innerHTML = 0
+    saved.innerHTML = 0
+})
+
